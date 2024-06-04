87b86a4db414b098edcdfa1308429f87fc5d3ed0
CSH2315 <choish00123@gmail.com>
2024-06-04 20:46:01 +0900

    Feat: 메시지 송신, 수신 함수 통합 - receive, send handling 함수를 socket_chatting 안에 집어넣음 - socket_chatting 함수 실행 시 채팅 가능
    ref: #2

--- port.py
+++ port.py
@@ -2,21 +2,7 @@ import socket
 import threading
 
 host='127.0.0.1'
-def handle_receive(conn):
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        print(f"Received: {data.decode()}")
-    conn.close()
 
-def handle_send(conn):
-    while True:
-        message = input("Enter message to send: ")
-        conn.sendall(message.encode())
-        if message.lower() == "bye":
-            break
-    conn.close()
 
 def init(user_id, port, connect_id):
     user_id = user_id.strip()
@@ -24,7 +10,24 @@ def init(user_id, port, connect_id):
     connect_id = str(connect_id) # 채팅을 보낼 상대 유저의 ID
     return user_id, port, connect_id
 
+# def socket_chatting(user_id, host, listen_port): this is test code
 def socket_chatting(user_id, host, listen_port, connect_id):
+    def handle_receive(conn):
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(f"Received: {data.decode()}")
+        conn.close()
+
+    def handle_send(conn):
+        while True:
+            message = input("Enter message to send: ")
+            conn.sendall(message.encode())
+            if message.lower() == "bye":
+                break
+        conn.close()
+
     # socket 연결 부분
     s_listen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s_listen.bind((host, listen_port))
@@ -32,6 +35,7 @@ def socket_chatting(user_id, host, listen_port, connect_id):
     print(f'Client {user_id} listening on {host}:{listen_port}')
 
     connect_port = int(find(user_list, connect_id)) # login_list.py에 있는 find 함수
+    # connect_port = int(input("connect port: ")) this is test code.
     s_connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s_connect.connect((host, connect_port))
@@ -53,7 +57,14 @@ def socket_chatting(user_id, host, listen_port, connect_id):
     receive_thread.join()
     send_thread.join()
 
-
+'''
+# this is test code 
+user_id = input('username: ')
+port = int(input('port: '))
+connect_id = input('connect id: ')
+user_id, port, connect_id = init(user_id, port, connect_id)
+socket_chatting(user_id, host, port)
+'''
 
 '''
 
